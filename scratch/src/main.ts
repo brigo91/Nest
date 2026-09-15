96077b1f3346806033dcc0af64236b6f00558e98
brigo <brigovaczbalazs@hotmail.com>
2026-09-15 21:04:58 +0200

    6. Starting Up a Nest App

--- scratch/src/main.ts
+++ scratch/src/main.ts
@@ -1,9 +1,22 @@
 import { Controller, Module, Get } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
 
 @Controller()
 export class AppController {
   @Get()
   getRootRoute(): string {
-    return 'Hello World!';
+    return 'hi there!';
   }
 }
+
+@Module({
+  controllers: [AppController],
+})
+export class AppModule {}
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+}
+
+bootstrap();
